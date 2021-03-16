c5ce897bd139cb048db5875456aeb6b86eb96c09
Michal Kozik <michal.kozikowski99@gmail.com>
2021-03-16 18:12:24 +0100

    Dodanie szyfru płotkowego.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int k = 3;
+        String word = "CRYPTOGRAPHY";
+
+        RailFence railFence = new RailFence();
+        String codedMessage = railFence.code(word, k);
+        System.out.println(codedMessage);
+        String decodedMessage = railFence.decode(codedMessage, k);
+        System.out.println(decodedMessage);
     }
 }
